222292e1cf6b6243eafc03aa34ba0e3814320f39
Alexander Sakhaev <alsakhaev@gmail.com>
2022-05-05 00:08:56 +0300

    fix: watch building is failed

--- dapplet/rollup.config.js
+++ dapplet/rollup.config.js
@@ -4,7 +4,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import fs from 'fs';
-import typescript from 'rollup-plugin-typescript2';
+import typescript from 'rollup-plugin-typescript';
 
 const showAddress = () => ({
   load: () =>
